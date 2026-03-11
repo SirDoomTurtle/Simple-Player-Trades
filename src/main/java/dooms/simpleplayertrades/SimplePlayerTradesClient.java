29d4f29c51850c3c70ca8c36cbb1359f5d290347
Doom Turtle <max55wawrina@gmail.com>
2026-03-11 09:18:35 +0100

    clean repo setup

---
+++ src/main/java/dooms/simpleplayertrades/SimplePlayerTradesClient.java
@@ -0,0 +1,11 @@
+package dooms.simpleplayertrades;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimplePlayerTradesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
